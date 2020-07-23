7497f2835c71c26c104a25eea177a3a1a542c707
Thiago Kienbaum <thiago.kienbaum@hotmail.com>
2020-07-23 20:43:28 -0300

    feat: created user controller and post/put routes

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,12 @@
 import { Router } from 'express';
+import UserController from './app/controllers/UserController';
 import StarredRepositoryController from './app/controllers/StarredRepositoryController';
 
 const routes = new Router();
 
+routes.post('/users', UserController.store);
 routes.get('/starredrepositories', StarredRepositoryController.index);
+
+routes.put('/users', UserController.update);
 
 export default routes;
