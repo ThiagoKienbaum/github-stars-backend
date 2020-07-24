2d11e900de4fcf82a0537b80bc7450828488057a
Thiago Kienbaum <thiago.kienbaum@hotmail.com>
2020-07-23 22:42:04 -0300

    fix: Add express async errors and youch to error treatment and fix import but on app

--- src/app.js
+++ src/app.js
@@ -1,10 +1,9 @@
 import './bootstrap';
-
-import Youch from 'youch';
 import express from 'express';
+import Youch from 'youch';
 import 'express-async-errors';
-
 import routes from './routes';
+import './database';
 
 class App {
   constructor() {
@@ -12,7 +11,6 @@ class App {
 
     this.middlewares();
     this.routes();
-    this.exceptionHandler();
   }
 
   middlewares() {
@@ -27,7 +25,6 @@ class App {
     this.server.use(async (err, req, res, next) => {
       if (process.env.NODE_ENV === 'development') {
         const errors = await new Youch(err, req).toJSON();
-
         return res.status(500).json(errors);
       }
 
