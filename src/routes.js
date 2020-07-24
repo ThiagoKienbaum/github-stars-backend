f744a20c988f88fc425ef43718cebee01055db13
Thiago Kienbaum <thiago.kienbaum@hotmail.com>
2020-07-23 21:32:32 -0300

    feat: created authentication middleware

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,18 @@
 import { Router } from 'express';
 import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
 import StarredRepositoryController from './app/controllers/StarredRepositoryController';
+import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 
 routes.post('/users', UserController.store);
-routes.get('/starredrepositories', StarredRepositoryController.index);
+routes.post('/session', SessionController.store);
+
+routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
+
+routes.get('/starredrepositories', StarredRepositoryController.index);
 
 export default routes;
